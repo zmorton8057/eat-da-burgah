d20d490cbcb83d5356697cd6648a52188e409751
Zac Morton <zmorton8057@gmail.com>
2019-09-22 15:25:21 -0700

    Setting up connection.js for JAWSDB heroku addon

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
-connection.connect(function(err){
-    if (err){
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+};
+
+connection.connect(function (err) {
+    if (err) {
         console.log("Error Connection: " + err.stack);
         return;
     }
